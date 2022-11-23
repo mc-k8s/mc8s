64bbd258dbb9abbed08b142dfe435279fa79c22f
Melvin Schneider <melvinschneider02@gmail.com>
2022-11-23 23:06:38 +0100

    changed bungeecord to true in spigot.yml

--- mc8s-gameserver/start_script.sh
+++ mc8s-gameserver/start_script.sh
@@ -8,6 +8,7 @@ if [[ $MC8S_DISABLE_END == "true" ]];
   then echo $'settings:\n  allow-end: false' >> bukkit.yml;
 fi
 
+echo $'settings:\n  bungeecord: true' >> spigot.yml;
 echo $'online-mode=false\n' >> server.properties
 cp -R /var/map-template/* .
 java -Dcom.mojang.eula.agree=true -jar spigot-1.19.2.jar
