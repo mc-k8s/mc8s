949dc49d9b763b43f24a875c9e1636153ace8974
Melvin Schneider <melvinschneider02@gmail.com>
2022-11-23 21:32:49 +0100

    Added end env

--- mc8s-gameserver/start_script.sh
+++ mc8s-gameserver/start_script.sh
@@ -4,4 +4,8 @@ if [[ $MC8S_DISABLE_NETHER == "true" ]];
   then echo "allow-nether=false\n" >> server.properties;
 fi
 
+if [[ $MC8S_DISABLE_END == "true" ]];
+  then echo "settings:\n  allow-end: false" >> bukkit.yml;
+fi
+
 java -Dcom.mojang.eula.agree=true -jar spigot-1.19.2.jar -o false
