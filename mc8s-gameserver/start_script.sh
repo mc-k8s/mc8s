a5ee88feac8096309c11e535dfb48b7748c8fccf
Melvin Schneider <melvinschneider02@gmail.com>
2022-11-23 21:26:16 +0100

    Fixed nether env

--- mc8s-gameserver/start_script.sh
+++ mc8s-gameserver/start_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$MC8S_DISABLE_NETHER" == "true"]
-  echo "allow-nether=false\n" >> server.properties
+if [[ $MC8S_DISABLE_NETHER == "true" ]];
+  then echo "allow-nether=false\n" >> server.properties;
 fi
 
 java -Dcom.mojang.eula.agree=true -jar spigot-1.19.2.jar -o false
