73ddf5274a673a33dd8df1440e5d06eb51bf6f26
Melvin Schneider <melvinschneider02@gmail.com>
2022-11-23 22:09:22 +0100

    Fixed Maptemplate-system

--- mc8s-gameserver/start_script.sh
+++ mc8s-gameserver/start_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ $MC8S_DISABLE_NETHER == "true" ]];
-  then echo "allow-nether=false\n" >> server.properties;
+  then echo "allow-nether=false" >> server.properties;
 fi
 
 if [[ $MC8S_DISABLE_END == "true" ]];
-  then echo "settings:\n  allow-end: false" >> bukkit.yml;
+  then echo $'settings:\n  allow-end: false' >> bukkit.yml;
 fi
 
 java -Dcom.mojang.eula.agree=true -jar spigot-1.19.2.jar -o false
