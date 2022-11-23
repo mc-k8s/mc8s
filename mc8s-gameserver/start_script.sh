f53ff920e728bfdecc12c899c5321d099628e970
Melvin Schneider <melvinschneider02@gmail.com>
2022-11-23 22:49:07 +0100

    Copy maps into work dir

--- mc8s-gameserver/start_script.sh
+++ mc8s-gameserver/start_script.sh
@@ -8,4 +8,5 @@ if [[ $MC8S_DISABLE_END == "true" ]];
   then echo $'settings:\n  allow-end: false' >> bukkit.yml;
 fi
 
+cp -R /var/map-template/* .
 java -Dcom.mojang.eula.agree=true -jar spigot-1.19.2.jar -o false
