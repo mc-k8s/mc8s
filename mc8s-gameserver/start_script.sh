4b5498cd6932ecbdbbcbecf9e5037fda4688d96f
Melvin Schneider <melvinschneider02@gmail.com>
2022-11-23 23:00:13 +0100

    Fixed onlinemode

--- mc8s-gameserver/start_script.sh
+++ mc8s-gameserver/start_script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [[ $MC8S_DISABLE_NETHER == "true" ]];
-  then echo "allow-nether=false" >> server.properties;
+  then echo $'allow-nether=false\n' >> server.properties;
 fi
 
 if [[ $MC8S_DISABLE_END == "true" ]];
   then echo $'settings:\n  allow-end: false' >> bukkit.yml;
 fi
 
+echo $'online-mode=false\n' >> server.properties
 cp -R /var/map-template/* .
-java -Dcom.mojang.eula.agree=true -jar spigot-1.19.2.jar -o false
+java -Dcom.mojang.eula.agree=true -jar spigot-1.19.2.jar
